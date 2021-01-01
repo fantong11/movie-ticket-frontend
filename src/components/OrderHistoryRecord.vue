52df89544fe4945340ad6aca914b497a4c199cc4
Fantong <fankaihsiang11@gmail.com>
2021-01-02 01:46:18 +0800

    Member頁面裡的table分成component

---
+++ src/components/OrderHistoryRecord.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="order-history-record">
+    <h3>訂單紀錄</h3>
+    <b-table :items="items" :fields="fields">
+      <template v-slot:cell(orderContent)>
+        取票序號：1111111111111111111111111111111111111111111111111111
+        <br>
+        影城：
+        <br>
+        電影：
+        <br>
+        場次：
+        <br>
+        總計金額：
+      </template>
+      <template v-slot:cell(details)>
+        <b-button size="md">詳細內容</b-button>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "OrderHistoryRecord",
+  data() {
+    return {
+      fields: [
+        {
+          key: "orderTime",
+          label: "訂購時間",
+        },
+        {
+          key: "orderContent",
+          label: "訂單內容",
+        },
+        {
+          key: "details",
+          label: "詳細內容"
+        }
+      ],
+      items: [
+        {
+          orderTime: "2020-1-1",
+        },
+        {
+          orderTime: "2020-1-1",
+        },
+        {
+          orderTime: "2020-1-1",
+        }
+      ]
+    };
+  },
+  computed: {
+    ...mapState({
+      orderList: (state) => state.order.orderList,
+    }),
+  },
+  mounted() {
+    //this.initOrder();
+  },
+  methods: {
+    initOrder() {
+      this.$store.dispatch("order/fetchUserOrder").then(() => {
+        console.log(this.orderList);
+      });
+    },
+  },
+}
+</script>
--- src/views/member.vue
+++ src/views/member.vue
@@ -1,48 +1,21 @@
 <template>
   <div class="member">
     <ResponsiveNavigation msg="Welcome to Your Vue.js App" />
-    <b-container class="border border-info text-left" fluid="lg">
-      <b-table :items="orderList" :fields="fields" responsive> </b-table>
+    <b-container class="text-left mt-5" fluid="lg">
+      <OrderHistoryRecord />
     </b-container>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
 import ResponsiveNavigation from "@/components/ResponsiveNavigation.vue";
+import OrderHistoryRecord from "@/components/OrderHistoryRecord.vue";
 export default {
   name: "member",
   components: {
     ResponsiveNavigation,
+    OrderHistoryRecord,
   },
-  data() {
-    return {
-      fields: [
-        {
-          key: "order_time",
-          label: "下訂時間",
-        },
-        {
-          key: "price",
-          label: "金額",
-        },
-      ],
-    };
-  },
-  computed: {
-    ...mapState({
-      orderList: (state) => state.order.orderList,
-    }),
-  },
-  mounted() {
-    this.initOrder();
-  },
-  methods: {
-    initOrder() {
-      this.$store.dispatch("order/fetchUserOrder").then(() => {
-        console.log(this.orderList);
-      });
-    },
-  },
+  
 };
 </script>
